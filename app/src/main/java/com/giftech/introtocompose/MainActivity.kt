b0b61d9a979897c260e65bb431a2e7a4179954e4
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-09-18 10:31:59 +0700

    ui: add text currency

--- app/src/main/java/com/giftech/introtocompose/MainActivity.kt
+++ app/src/main/java/com/giftech/introtocompose/MainActivity.kt
@@ -3,10 +3,8 @@ package com.giftech.introtocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.Surface
@@ -15,8 +13,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.giftech.introtocompose.ui.theme.IntroToComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,20 +38,33 @@ fun MyApp() {
         modifier = Modifier.fillMaxSize(),
         color = Color(0xFF546E7A)
     ) {
-
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                "$100",
+                style = TextStyle(color = Color.White, fontSize = 32.sp, fontWeight = FontWeight.Bold)
+            )
+            Spacer(Modifier.height(16.dp))
+            CreateCircle()
+        }
     }
 }
 
-@Preview
 @Composable
 fun CreateCircle() {
     Card(
         modifier = Modifier
-            .padding(3.dp)
-            .size(45.dp),
+            .padding(8.dp)
+            .size(145.dp)
+            .clickable {
+
+            },
         shape = CircleShape,
+        elevation = 4.dp
     ) {
-        Box(contentAlignment = Alignment.Center){
+        Box(contentAlignment = Alignment.Center) {
             Text(text = "Tap", modifier = Modifier)
         }
     }
