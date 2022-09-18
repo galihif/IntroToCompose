2ff847023c2e2497ba3367e794ac3640598b49e5
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-09-18 09:20:39 +0700

    ui: create component circle

--- app/src/main/java/com/giftech/introtocompose/MainActivity.kt
+++ app/src/main/java/com/giftech/introtocompose/MainActivity.kt
@@ -3,13 +3,20 @@ package com.giftech.introtocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.giftech.introtocompose.ui.theme.IntroToComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +25,41 @@ class MainActivity : ComponentActivity() {
         setContent {
             IntroToComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xFF546E7A)
+    ) {
+
+    }
+}
+
+@Preview
+@Composable
+fun CreateCircle() {
+    Card(
+        modifier = Modifier
+            .padding(3.dp)
+            .size(45.dp),
+        shape = CircleShape,
+    ) {
+        Box(contentAlignment = Alignment.Center){
+            Text(text = "Tap", modifier = Modifier)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     IntroToComposeTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
